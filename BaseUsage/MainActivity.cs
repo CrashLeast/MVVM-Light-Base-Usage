c4019ec81277a2b1fa52e5af0f283a62bb6bb973
T5000er <1370596902@qq.com>
2018-04-16 17:15:58 +0800

    MVVM Light Demo

---
+++ BaseUsage/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using GalaSoft.MvvmLight.Views;
+using GalaSoft.MvvmLight.Helpers;
+
+namespace BaseUsage
+{
+    [Activity(Label = "BaseUsage", MainLauncher = true)]
+    public class MainActivity : ActivityBase
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            EditText mEtInput = FindViewById<EditText>(Resource.Id.et_input);
+            TextView mTvOutput = FindViewById<TextView>(Resource.Id.tv_output);
+
+            this.SetBinding(()=>mEtInput.Text,()=>mTvOutput.Text);
+        }
+    }
+}
+
---
+++ BaseUsage/obj/Debug/android/src/md5884a142889809f31be2acdb59b6b54b7/MainActivity.java
@@ -0,0 +1,47 @@
+package md5884a142889809f31be2acdb59b6b54b7;
+
+
+public class MainActivity
+	extends md54ed5d3d82bc760df3f16570e3e2f7544.ActivityBase
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("BaseUsage.MainActivity, BaseUsage, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("BaseUsage.MainActivity, BaseUsage, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
